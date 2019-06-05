eb0889eb427538cccdd03aa1050fca75e84a6198
ado1745 <ado1745@gmail.com>
2019-06-05 07:37:21 -0400

    mysql activities in class updated

--- activities/12-mysql/01-Activities/06-iceCreamWithConnection/iceCreamDBConnection.js
+++ activities/12-mysql/01-Activities/06-iceCreamWithConnection/iceCreamDBConnection.js
@@ -4,17 +4,17 @@ var connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 3306,
+  port: 8889,
 
   // Your username
   user: "root",
 
   // Your password
-  password: "",
+  password: "root",
   database: "ice_creamDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   connection.end();
