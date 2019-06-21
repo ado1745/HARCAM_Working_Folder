7d24824825479065dcda6c1d7bd1718cc15b877b
ado1745 <ado1745@gmail.com>
2019-06-20 23:19:06 -0400

    updated handlebards calc activity

--- activities/14-handlebars/01-Activities/01-ExpressCalculator/Unsolved/expressCalculator.js
+++ activities/14-handlebars/01-Activities/01-ExpressCalculator/Unsolved/expressCalculator.js
@@ -1,43 +1,50 @@
 // Dependencies
-var express = require("express");
+let express = require("express");
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 
 // Create express app instance.
-var app = express();
+let app = express();
 
 // Routes
 // What routes do you need to have? Which ones are optional?
 // TODO Add your routes here
-app.get("", function(req, res) {
+app.get("/:operator/:operand1/:operand2", function (req, res) {
 
   // TODO parse out the variables from the request
   // Parameters are received from the URL
   // TODO make sure they're converted to integers (and not strings)
   // Parameters are converted to integers
 
+  let operand1 = +req.params.operand1;
+  let operand2 = +req.params.operand2;
+  let operation = req.params.operator;
   // Initialize the result variable to send later
-  var result;
+  let result;
   // Switch statement chooses operation based on the operation parameter.
   switch (operation) {
-  // BONUS - How could you use * + etc. inside the app.get()?
-  case "add":
-    // Add your logic here. Pun intended.
-    break;
-  case "subtract":
-    // Subtract logic
-    break;
-  case "multiply":
-    // Multiply
-    break;
-  case "divide":
-    // Divide
-    break;
-  default:
-    // Handle anything that isn't specified
-    result =
+    // BONUS - How could you use * + etc. inside the app.get()?
+    case "add":
+      // Add your logic here. Pun intended.
+      result = operand1 + operand2;
+      break;
+    case "subtract":
+      // Subtract logic
+      result = operand1 - operand2;
+      break;
+    case "multiply":
+      // Multiply
+      result = operand1 * operand2;
+      break;
+    case "divide":
+      // Divide
+      result = operand1 / operand2;
+      break;
+    default:
+      // Handle anything that isn't specified
+      result =
         "Sorry! The only valid operations are add, subtract, multiply, and divide.";
   }
 
@@ -47,7 +54,7 @@ app.get("", function(req, res) {
 });
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
